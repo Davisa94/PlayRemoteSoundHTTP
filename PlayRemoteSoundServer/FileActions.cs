1b52a42551f902abbe5b99752023521627ffb6da
Davisa94 <Austin.i.davis94@gmail.com>
2021-09-08 22:22:42 -0700

    Added sound and file libraries I wrote ANd changed target to windows only

---
+++ PlayRemoteSoundServer/FileActions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayRemoteSoundServer
+{
+    public class FileActions
+    {
+        public static string ConcatFilenameToPath(string filename, string path)
+        {
+            var finalPath = $"{path}\\{filename}";
+            return finalPath;
+        }
+        public string GetMessage(string fileName)
+        {
+            string fileContents = System.IO.File.ReadAllText(fileName);
+            return fileContents;
+        }
+        public void EditFile(string filename, string message)
+        {
+            // what line in the html do we write the action to
+            int editLine = 116;
+            // path to the html file
+            string lineToWrite = null;
+            // verify that the line is valid
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                for (int i = 1; i <= editLine; ++i)
+                    lineToWrite = reader.ReadLine();
+            }
+
+            if (lineToWrite == null)
+                throw new InvalidDataException("Line Number" + editLine + " does not exist in " + filename);
+
+            // read the whole file:
+            string[] lines = File.ReadAllLines(filename);
+
+            // Write the new file over the old file.
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                for (int currentLine = 1; currentLine <= lines.Length; ++currentLine)
+                {
+                    if (currentLine == editLine)
+                    {
+                        writer.WriteLine(lineToWrite);
+                    }
+                    else
+                    {
+                        writer.WriteLine(lines[currentLine - 1]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PlayRemoteSoundServer/PlayLocalSound.cs
@@ -0,0 +1,39 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PlayRemoteSoundServer
+{
+    public class PlayLocalSound
+    {
+        public static bool Play(string filename)
+        {
+            SoundPlayer player = new()
+            {
+                SoundLocation = filename
+            };
+            player.Load();
+            player.Play();
+            return true;
+        }
+        public static void PlayAll(string filename)
+        {
+            using (var audioFile = new AudioFileReader(filename))
+            using (var outputDevice = new WaveOutEvent())
+            {
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+
+}
