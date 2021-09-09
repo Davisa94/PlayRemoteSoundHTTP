80c07232c0b045fabb525d94f1952c94ada1041b
Davisa94 <Austin.i.davis94@gmail.com>
2021-09-08 22:15:22 -0700

    Initial commit

---
+++ PlayRemoteSoundServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayRemoteSoundServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
